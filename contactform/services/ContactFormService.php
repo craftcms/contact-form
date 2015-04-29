b34aeaf957196560648aedb2bb990c321780b6fc
Nate Iler <nate@flipboxdigital.com>
2015-04-29 12:34:08 -0600

    Added ability to edit subject and message via '/admin/settings/email/messages'

--- contactform/services/ContactFormService.php
+++ contactform/services/ContactFormService.php
@@ -33,28 +33,37 @@ class ContactFormService extends BaseApplicationComponent
 			{
 				$toEmails = ArrayHelper::stringToArray($settings->toEmail);
 
+                // translation subject / body
+                $translationSubject = Craft::t('contactform_subject', null, null, 'en_us');
+                $translationBody = Craft::t('contactform_body', null, null, 'en_us');
+
+                // prepend message subject?
+                $message->subject = $settings->prependSubject . ($settings->prependSubject && $message->subject ? ' - ' : '') . $message->subject;
+
+                // get email settings
+                $emailSettings = craft()->email->getSettings();
+
 				foreach ($toEmails as $toEmail)
 				{
 					$email = new EmailModel();
-					$emailSettings = craft()->email->getSettings();
 
 					$email->fromEmail = $emailSettings['emailAddress'];
 					$email->replyTo   = $message->fromEmail;
 					$email->sender    = $emailSettings['emailAddress'];
 					$email->fromName  = $settings->prependSender . ($settings->prependSender && $message->fromName ? ' ' : '') . $message->fromName;
 					$email->toEmail   = $toEmail;
-					$email->subject   = $settings->prependSubject . ($settings->prependSubject && $message->subject ? ' - ' : '') . $message->subject;
-					$email->body      = $message->message;
+					$email->subject   = $translationSubject;
+					$email->body      = $translationBody;
 
 					if ($message->attachment)
 					{
 						$email->addAttachment($message->attachment->getTempName(), $message->attachment->getName(), 'base64', $message->attachment->getType());
 					}
 
-					craft()->email->sendEmail($email);
+					craft()->email->sendEmail($email, $message->getAttributes());
+
 				}
 			}
-
 			return true;
 		}
 
---
+++ contactform/translations/en_us.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+
+	'contactform_heading' => 'Contact Form',
+	'contactform_subject' => '{{subject}}',
+	'contactform_body' => '{{message}}'
+
+);
