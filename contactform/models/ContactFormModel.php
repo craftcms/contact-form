3d83f7eb4819666ae121387fd6328b7bacd88fcf
Jason McCallister <themccallister@gmail.com>
2015-10-10 01:19:04 -0700

    Make fromName required

--- contactform/models/ContactFormModel.php
+++ contactform/models/ContactFormModel.php
@@ -6,7 +6,7 @@ class ContactFormModel extends BaseModel
 	protected function defineAttributes()
 	{
 		return array(
-			'fromName'   => array(AttributeType::String, 'label' => 'Your Name'),
+			'fromName'   => array(AttributeType::String, 'required' => true, 'label' => 'Your Name'),
 			'fromEmail'  => array(AttributeType::Email,  'required' => true, 'label' => 'Your Email'),
 			'message'    => array(AttributeType::String, 'required' => true, 'label' => 'Message'),
 			'subject'    => array(AttributeType::String, 'label' => 'Subject'),
