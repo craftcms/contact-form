11a422d748b503a305cd9a44eba0b2031c6bb869
rolandbrand <r.brand@iwf.ch>
2017-09-26 17:00:17 +0200

    Added german translations

---
+++ src/translations/de/contact-form.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    //model
+    "To Email" => "Empfängeradresse",
+    'Your Name' => "Ihr Name",
+    'Your Email' => "Ihre Email",
+    'Message' => "Nachricht",
+    'Subject' => "Betreff",
+
+    //plugin settings
+    "The email address(es) that the contact form will send to. Separate multiple email addresses with commas."
+        => "Die Email-Adresse(n) an welche das Kontaktformular senden wird. Trennen Sie mehrere Email-Adressen mit Kommas.",
+    "Sender Text"
+        => "Absendertext",
+    "Text that will be prepended to the email's From Name to inform who the Contact Form actually was sent by."
+        => "Text, welcher dem Absendernamen der Email vorangestellt wird, um aufzuzeigen von wem das Kontaktformular abgesendet wurde.",
+    "On behalf of"
+        => "Mitteilung von",
+    "Subject Text"
+        => "Betreffzeile",
+    "Text that will be prepended to the email's Subject to flag that it comes from the Contact Form."
+        => "Text, welcher der Betreffzeile vorangestellt wird, um aufzuzeigen dass die Mail vom Kontaktformular stammt",
+    "Allow attachments?"
+        => "Anlagen erlauben?",
+    "Success Flash Message"
+        => "Erfolgsnachricht",
+    "The flash message diplayed after successfully sending a message."
+        => "Die Statusnachricht welche angezeigt wird, wenn eine Nachricht erfolgreich versendet wurde.",
+    "Your message has been sent."
+        => "Ihre Nachricht wurde versendet."
+];
