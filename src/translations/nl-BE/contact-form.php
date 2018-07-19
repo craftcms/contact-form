df9d7439e9841a93839e7c98551363092cf31b9c
Michael Thomas <michael@dadamotion.be>
2018-07-19 10:54:16 +0200

    added Dutch-Belgian translations

---
+++ src/translations/nl-BE/contact-form.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    //model
+    "To Email" => "E-mail naar",
+    'Your Name' => "Jouw naam",
+    'Your Email' => "Jouw e-mailadres",
+    'Message' => "Bericht",
+    'Subject' => "Onderwerp",
+
+    //plugin settings
+    "The email address(es) that the contact form will send to. Separate multiple email addresses with commas."
+    => "Het e-mailadres of de e-mailadressen naar waar het contactformulier dient te verzenden. Scheid meerdere e-mailadressen met komma's.",
+    "Sender Text"
+    => "Afzendersbericht",
+    "Text that will be prepended to the email's From Name to inform who the Contact Form actually was sent by."
+    => "Tekst die wordt toegevoegd aan de naam van de verzender om te informeren door wie het contactformulier is verzonden.",
+    "On behalf of"
+    => "Namens",
+    "Subject Text"
+    => "Onderwerp tekst",
+    "Text that will be prepended to the email's Subject to flag that it comes from the Contact Form."
+    => "Tekst die zal worden toegevoegd aan het onderwerp om te tonen dat het afkomstig is van het contactformulier.",
+    "Allow attachments?"
+    => "Bijlagen toestaan?",
+    "Success Flash Message"
+    => "Statusbericht geslaagde verzending",
+    "The flash message diplayed after successfully sending a message."
+    => "Het statusbericht dat wordt getoond na het succesvol versturen van een bericht.",
+    "Your message has been sent."
+    => "Jouw bericht is verstuurd."
+];
