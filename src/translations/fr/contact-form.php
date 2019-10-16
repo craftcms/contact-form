6e1b120de7e6e5386cc3117caf12134f3d78b152
Mehdi Achour <machour@gmail.com>
2019-10-16 19:24:32 +0100

    Add french translations

---
+++ src/translations/fr/contact-form.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    //model
+    "To Email" => "Destinataire",
+    'Your Name' => "Votre nom",
+    'Your Email' => "Votre email",
+    'Message' => "Message",
+    'Subject' => "Sujet",
+
+    //error messages
+    'There was a problem with your submission, please check the form and try again!'
+    => 'Une erreur est survenue lors de la soumission, vérifiez le formulaire et essayez à nouveau !',
+
+    //plugin settings
+    "The email address(es) that the contact form will send to. Separate multiple email addresses with commas."
+    => "La ou les adresses qui recevront les emails. Séparez les adresses multiples par des virgules.",
+    "Sender Text" => "Texte expéditeur",
+    "Text that will be prepended to the email’s From Name to inform who the Contact Form actually was sent by."
+    => "Texte qui sera ajouté avant le nom de l'expéditeur pour indiquer par qui le formulaire de contact a été envoyé.",
+    "On behalf of" => "De la part de",
+    "Subject Text" => "Texte sujet",
+    "Text that will be prepended to the email’s Subject to flag that it comes from the Contact Form."
+    => "Texte qui sera ajouté au début du sujet du mail pour indiquer qu'il a été envoyé depuis le formulaire de contact.",
+    "Allow attachments?" => "Autoriser les pièces jointes ?",
+    "Success Flash Message" => "Message de succès",
+    "The flash message displayed after successfully sending a message."
+    => "Le message affiché après avoir envoyé un message avec succès.",
+    "Your message has been sent." => "Votre message a été envoyé."
+];
