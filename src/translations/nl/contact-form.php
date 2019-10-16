03e9ee132daea7384e16fc74eceae35f88c94bbe
Mehdi Achour <machour@gmail.com>
2019-10-16 19:27:30 +0100

    Fix original messages

--- src/translations/nl/contact-form.php
+++ src/translations/nl/contact-form.php
@@ -13,19 +13,19 @@ return [
     => "Het e-mailadres of de e-mailadressen naar waar het contactformulier dient te verzenden. Scheid meerdere e-mailadressen met komma's.",
     "Sender Text"
     => "Afzendersbericht",
-    "Text that will be prepended to the email's From Name to inform who the Contact Form actually was sent by."
+    "Text that will be prepended to the email’s From Name to inform who the Contact Form actually was sent by."
     => "Tekst die wordt toegevoegd aan de naam van de verzender om te informeren door wie het contactformulier is verzonden.",
     "On behalf of"
     => "Namens",
     "Subject Text"
     => "Onderwerp tekst",
-    "Text that will be prepended to the email's Subject to flag that it comes from the Contact Form."
+    "Text that will be prepended to the email’s Subject to flag that it comes from the Contact Form."
     => "Tekst die zal worden toegevoegd aan het onderwerp om te tonen dat het afkomstig is van het contactformulier.",
     "Allow attachments?"
     => "Bijlagen toestaan?",
     "Success Flash Message"
     => "Statusbericht geslaagde verzending",
-    "The flash message diplayed after successfully sending a message."
+    "The flash message displayed after successfully sending a message."
     => "Het statusbericht dat wordt getoond na het succesvol versturen van een bericht.",
     "Your message has been sent."
     => "Jouw bericht is verstuurd."
