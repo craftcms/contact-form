8401cc67ef2f8c3a9af25de0b009cd61895ad9cb
Rafrobeat <rafaeldiazc@me.com>
2020-04-03 20:35:45 -0500

    Spanish traslate

---
+++ src/translations/es/contact-form.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    //model
+    "To Email" => "Destinatario",
+    'Your Name' => "Su Nombre",
+    'Your Email' => "Su email",
+    'Message' => "Mensaje",
+    'Subject' => "Asunto",
+
+    //error messages
+    'There was a problem with your submission, please check the form and try again!'
+    => 'Hubo un problema con su envío, por favor revise el formulario y vuelva a intentarlo!',
+
+    //plugin settings
+    "The email address(es) that the contact form will send to. Separate multiple email addresses with commas."
+    => "La(s) direccion(es) de email a las que se enviará el email de contacto. Separa las distintas direcciones de email con comas.",
+    "Sender Text" => "Texto del remitente",
+    "Text that will be prepended to the email’s From Name to inform who the Contact Form actually was sent by."
+    => "Texto que se antepondrá al Nombre del remitente del email para informar a quién se envió realmente el correo de contacto.",
+    "On behalf of" => "A nombre de",
+    "Subject Text" => "Texto del asunto",
+    "Text that will be prepended to the email’s Subject to flag that it comes from the Contact Form."
+    => "Texto que se agregará previamente al asunto del correo para informar que proviene del formulario de contacto.",
+    "Allow attachments?" => "Permitir adjuntos?",
+    "Success Flash Message" => "Mensaje flash para correos enviados",
+    "The flash message displayed after successfully sending a message."
+    => "El mensaje flash que se muestra después de enviar un mensaje correctamente.",
+    "Your message has been sent." => "Su mensaje ha sido enviado."
+];
